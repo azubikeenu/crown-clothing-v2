de2e4480c46e3e8fc681496ff8252da3801fa37b
Enu Azubike <enuazubike88@gmail.com>
2022-08-05 00:00:02 +0100

    added reselect lib for memoization of the categories selector to prevent category component re-rendering

--- src/store/categories/cartegories.selector.js
+++ src/store/categories/cartegories.selector.js
@@ -1,6 +1,29 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from 'reselect';
+
+//Initial value of the reducer
+
+const categoriesReducer = (state) =>{
+  return  state.categories;
+  }
+
+// memoize the categories - this function returns a cached state of the categories in the categories reducer
+// it means as longs as the categoriesReducer doesnt change do not create a new categories array
+const cachedCategories = createSelector(
+  [categoriesReducer],
+  (categoriesSlice) => {
+    return categoriesSlice.categories}
+);
+
+// this memoizes the reduce function since it returns a new value each time it is called
+// as long as the categories does not change , do not run the reduce method
+export const selectCategoriesMap = createSelector(
+  [cachedCategories],
+  (categories) =>{
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+  }
+
+);
--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,21 @@ import { rootReducer } from './root-reducer';
 
 const middlewares = [logger];
 
+// const customLogger = (store) => (next) => (action) => {
+//   if (!action.type) {
+//     return next(action);
+//   }
+//   console.log('initial state', store.getState());
+//   console.log('payload', action.payload);
+//   console.log('type', action.type);
+//   next(action);
+//   console.log('next state', store.getState());
+//   console.log('payload', action.payload);
+//   console.log('type', action.type);
+// };
+
+
+
 // in other for the middlewares to work you must compose the middlewares
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
